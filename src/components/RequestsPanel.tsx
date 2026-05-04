19a9f31cb89b126ecedc7cf560590aeb1b0242ae
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-04 16:17:35 -0300

    Adicionado o preço no requestsPanel

--- src/components/RequestsPanel.tsx
+++ src/components/RequestsPanel.tsx
@@ -1,12 +1,18 @@
 export function RequestsPanel() {
   return (
-    <div className="flex items-center gap-3 m-0 p-0">
+    <div className="flex items-center gap-3 p-0 w-full">
       <div className="bg-gray-400 w-8.5 h-8.5 rounded-full">
 
       </div>
-      <div className="flex flex-col">
-        <div className="text-sm font-bold">Rodrigo</div>
-        <div className="text-xs text-gray-700">Alimentação</div>
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col">
+          <div className="text-sm font-bold">Rodrigo</div>
+          <div className="text-xs text-gray-700">Alimentação</div>
+        </div>
+        <div className="flex items-center">
+          <p>R$</p>
+          <p>25,89</p>
+        </div>
       </div>
     </div>
   )
