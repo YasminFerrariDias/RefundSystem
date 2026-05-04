8e26d0fccf8ea4103dade712da97633abb7088b3
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-04 16:10:39 -0300

    Esboço do RequestsPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { CardContainer } from "./components/CardContainer/CardContainer";
+import { RequestsPanel } from "./components/RequestsPanel";
 
 export function App() {
   return (
     <div className="bg-gray-400 flex w-auto h-screen">
-      <CardContainer size="lg" />
+      <CardContainer size="xl">
+        <RequestsPanel />
+      </CardContainer>
     </div>
   )
 }
---
+++ src/components/RequestsPanel.tsx
@@ -0,0 +1,13 @@
+export function RequestsPanel() {
+  return (
+    <div className="flex items-center gap-3 m-0 p-0">
+      <div className="bg-gray-400 w-8.5 h-8.5 rounded-full">
+
+      </div>
+      <div className="flex flex-col">
+        <div className="text-sm font-bold">Rodrigo</div>
+        <div className="text-xs text-gray-700">Alimentação</div>
+      </div>
+    </div>
+  )
+}
