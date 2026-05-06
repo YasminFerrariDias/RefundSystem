11fa2651564a65fffd3ca1cbb5e533d1a841aaf1
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-06 10:27:54 -0300

    Criação do Layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 import './styles/index.css'
+import { Layout } from './layout/layout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </StrictMode>,
 )
