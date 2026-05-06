547a3e28ac0d400314b565b089babc607b29dd6c
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-06 16:12:09 -0300

    Instalação do dialog

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Home } from "./pages/Home/home";
 
 export function App() {
   return (
-    <div className="bg-gray-400" h-full>
+    <div className="bg-gray-400 h-full" >
       <Home />
     </div>
   )
