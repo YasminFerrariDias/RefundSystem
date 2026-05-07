a2abc8d0b54955821a4ce41e28244df8e18a04b4
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-07 14:33:14 -0300

    Atualização de rotas no header

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -3,22 +3,27 @@ import { Icon } from "../../components/Icon/Icon";
 import { Text } from "../../components/Text/Text";
 import { NavLink } from "../../components/NavLink";
 import { ButtonContainer } from "../../components/Button/ButtonContainer/ButtonContainer";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 export function Header() {
   return (
     <div className="bg-gray-400 w-full p-10 pl-35 pr-35 flex justify-between">
-      <div className="flex gap-1 items-center">
-        <Icon icon={HiArrowUturnRight} size="md" iconColor="green100"></Icon>
-        <Text size="lg" decoration="semibold" textColor="green100">refund</Text>
-      </div>
+
+      <Link to="/">
+        <div className="flex gap-1 items-center">
+          <Icon icon={HiArrowUturnRight} size="md" iconColor="green100"></Icon>
+          <Text size="lg" decoration="semibold" textColor="green100">refund</Text>
+        </div>
+      </Link>
+
       <div className="flex gap-8 items-center">
-        <NavLink link="#" text="Solicitação de reembolso" isActive />
-    
+        <Link to="/">
+          <NavLink link="#" text="Solicitação de reembolso" isActive />
+        </Link>
 
         <Link to="/NewRefund">
           <ButtonContainer text="Nova Solicitação" textColor="white" decoration="bold" />
-        </Link>      
-        </div>
+        </Link>
+      </div>
     </div>
   )
 }
