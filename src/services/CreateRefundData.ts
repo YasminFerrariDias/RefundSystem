7cb3b105265eae8e30cb64cb544e1108359c3f93
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-14 15:00:25 -0300

    Apagando o createUpdateTYep

--- src/services/CreateRefundData.ts
+++
@@ -1,6 +0,0 @@
-export interface CreateRefundData {
-  title: string
-  category: string
-  value: number
-  receipt?: string | null
-}
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import type { CreateRefundData } from "./CreateRefundData"
+import type { Refund } from "../types/refund"
 
 const api = axios.create({ baseURL: import.meta.env.VITE_API_URL})
 
@@ -7,7 +7,7 @@ export const ApiRefunds = {
   getAll: () => api.get("/refunds"),
   getOne: (id: string) => api.get(`/refunds/${id}`),  
   getSearch: (q: string, page: number = 1) => api.get(`/refunds?q=${q}&page=${page}`),  
-  postCreate: (data: CreateRefundData) => api.post("/refunds", data),
+  postCreate: (data: Refund) => api.post("/refunds", data),
   deleteRefund: (id: string) => api.delete(`/refunds/${id}`),
 }
 
