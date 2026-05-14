13850fb943ab092631bff6d59be50e7531312889
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-14 14:09:17 -0300

    Foi altrado uma rota na tela do app

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ export function App() {
           <Route path='/' element={<RefundList />} />
           <Route path='/NewRefund' element={<NewRefund />} />
           <Route path='/RequestSent' element={<RequestSent />} />
-          <Route path='/DetailsRefund' element={<DetailsRefund />} />
           <Route path='/DetailsRefund/:id' element={<DetailsRefund />} />
         </Routes>
       </div>
